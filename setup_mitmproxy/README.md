942613cc28e38220365cd13035a1cd3715d04352
Midori_Tanaka <m_tanaka@waku-2.com>
2021-09-14 12:58:24 +0900

    feat: setup_mitmproxy.sh を追加

---
+++ setup_mitmproxy/README.md
@@ -0,0 +1,12 @@
+# mitmproxy
+This shell set up port used by proxy server.
+
+default port usage:
+```console
+zsh setup_mitmproxy.sh on
+```
+
+specify port to usage:
+```console
+zsh setup_mitmproxy.sh on <Number>
+```
---
+++ setup_mitmproxy/setup_mitmproxy.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Change mitmproxy wifi settings.
+# Usage : zsh setup_mitmproxy.sh <switch> <port:Number>
+if [ "$#" -eq 2 -a "$1" = "on" ]; then
+  input=$2
+  expr "$input" + 1 >&/dev/null
+  ret=$?
+  if [ $ret -lt 2 ];then
+    # networksetup -setwebproxy Wi-Fi "" $input
+    # networksetup -setsecurewebproxy Wi-Fi "" $input
+    echo "U can use poat :$input"
+    exit 0
+  else
+    echo "ポートの指定が不正です。"
+    echo "Usage : zsh setup_mitmproxy.sh on <Number>"
+    exit 1
+  fi
+fi
+
+if [ "$#" -eq 1 -a "$1" = "on" ]; then
+  networksetup -setwebproxy Wi-Fi "" 8080
+  networksetup -setsecurewebproxy Wi-Fi "" 8080
+  echo "Using default port :8080"
+elif [ "$#" -eq 1 -a "$1" = "off" ]; then
+  networksetup -setwebproxystate Wi-fi off
+  networksetup -setsecurewebproxystate Wi-fi off
+  echo "ポートの設定を解除しました。"
+else
+  echo "Usage : zsh setup_mitmproxy.sh <on|off> (port:Number)"
+  exit 1
+fi
